6d5465cec65904ea89c558f5001a6c8125cb9e7f
Hugo Dutka <contact@hugodutka.com>
2021-02-11 14:47:20 +0000

    specify tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Helvetica", "Arial", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
