a2762d052e65cfa8c410cb07da8e1c9819de037b
Hugo Dutka <contact@hugodutka.com>
2021-02-12 20:05:49 +0000

    implement a big chunk of the desktop navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,117 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql, Link } from "gatsby";
 import Img, { FixedObject } from "gatsby-image";
 
-export interface Props {
-  fixed: FixedObject;
+enum LinkVariant {
+  Red = "red",
+  Black = "black",
 }
 
-export const NavbarTemplate = ({ fixed }: Props) => (
-  <div className="text-lg">
-    <Img fixed={fixed} alt="Site logo" />
-  </div>
+const getLinkClasses = (variant: LinkVariant): String => {
+  if (variant == LinkVariant.Red) {
+    return "hover:text-black text-red-500";
+  } else if (variant == LinkVariant.Black) {
+    return "hover:text-red-500 text-black";
+  } else {
+    return "";
+  }
+};
+
+const getButtonClasses = (variant: LinkVariant): String => {
+  if (variant == LinkVariant.Red) {
+    return "text-red-500";
+  } else if (variant == LinkVariant.Black) {
+    return "text-black";
+  } else {
+    return "";
+  }
+};
+
+export interface Props {
+  imgFixed: FixedObject;
+  navbarLinks: Array<{
+    displayName: string;
+    variant: LinkVariant;
+    path: string;
+  }>;
+  navbarButtons: Array<{
+    displayName: string;
+    variant: LinkVariant;
+    path: string;
+  }>;
+}
+
+export const NavbarTemplate = ({
+  imgFixed,
+  navbarLinks,
+  navbarButtons,
+}: Props) => (
+  <header className="flex flex-row items-center justify-between pl-4 h-16 shadow">
+    <Link to="/" className="h-full flex flex-row items-center">
+      <Img fixed={imgFixed} alt="Site logo" />
+    </Link>
+    <div
+      className={`h-full hidden lg:flex flex-row items-center uppercase font-bold
+                  tracking-wide`}
+    >
+      {navbarLinks.map((link) => (
+        <Link
+          className={`${getLinkClasses(link.variant)} pr-10`}
+          to={link.path}
+        >
+          {link.displayName}
+        </Link>
+      ))}
+      {navbarButtons.map((button) => (
+        <Link
+          className={`h-full flex flex-row items-center border-gray-200 hover:bg-gray-200
+                      border-l px-6 ${getButtonClasses(button.variant)}`}
+          to={button.path}
+        >
+          {button.displayName}
+        </Link>
+      ))}
+    </div>
+  </header>
 );
 
 export const Navbar = () => (
   <StaticQuery
     query={componentQuery}
-    render={(data) => <NavbarTemplate {...data.file.childImageSharp} />}
+    render={(data) => (
+      <NavbarTemplate
+        imgFixed={data.file.childImageSharp.fixed}
+        navbarLinks={data.allGlobalYaml.nodes
+          .map((node) => node.navbarLinks)
+          .flat(1)}
+        navbarButtons={data.allGlobalYaml.nodes
+          .map((node) => node.navbarButtons)
+          .flat(1)}
+      />
+    )}
   />
 );
 
 const componentQuery = graphql`
-  query NavbarQuery {
+  query Navbar {
     file(relativePath: { eq: "images/uploads/logo.png" }) {
       childImageSharp {
-        fixed(height: 50) {
+        fixed(height: 32) {
           ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    allGlobalYaml {
+      nodes {
+        navbarLinks {
+          displayName
+          variant
+          path
+        }
+        navbarButtons {
+          displayName
+          variant
+          path
         }
       }
     }
