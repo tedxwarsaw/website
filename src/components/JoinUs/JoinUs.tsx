14a03d2c2e2e263e8b77efc18be07b5f550ca8b9
Kamil Sikora <sikorakamil26@gmail.com>
2021-04-13 16:23:53 +0200

    added types

--- src/components/JoinUs/JoinUs.tsx
+++ src/components/JoinUs/JoinUs.tsx
@@ -1,1 +1,20 @@
+import { FluidObject } from "gatsby-image";
 import React from "react";
+
+interface JoinUsProps {
+  joinUsTitle: string;
+  joinUsSubtitle: string;
+  joinUsImage: FluidObject;
+  joinUsImageDesktop: FluidObject;
+  joinUsVolonteerText: string;
+  joinUsVolonteerLink: string;
+  joinUsGetToKnowOurTeemText: string;
+  joinUsGetToKnowOurTeemLink: string;
+  joinUsBecomeSpeakerText: string;
+  joinUsBecomeSpeakerLink: string;
+}
+
+export const JoinUs = (props: JoinUsProps) => {
+  console.log(props);
+  return <p>join us</p>;
+};
