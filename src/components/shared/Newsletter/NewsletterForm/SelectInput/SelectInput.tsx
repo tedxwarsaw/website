21b0385aeba240edbd874ac52436f7c6734af0c4
Kamil Sikora <sikorakamil26@gmail.com>
2021-04-15 12:37:05 +0200

    styled select

--- src/components/shared/Newsletter/NewsletterForm/SelectInput/SelectInput.tsx
+++ src/components/shared/Newsletter/NewsletterForm/SelectInput/SelectInput.tsx
@@ -6,6 +6,11 @@ export const SelectInput = () => {
   const [isSelectActive, setIsSelectActive] = useState(false);
   const [selectValue, setSelectValue] = useState("Selected topic");
 
+  const handleSelect = (value: string) => {
+    setSelectValue(value);
+    setIsSelectActive(false);
+  };
+
   return (
     <div className="my-3 border border-customGrey rounded-md md:col-start-2 md:col-end-3 xl:mx-4">
       <div className="select-box p-4 ">
@@ -15,23 +20,29 @@ export const SelectInput = () => {
           }`}
         >
           <div
-            className="option px-4 py-2"
-            onClick={() => setSelectValue("New events")}
+            className="option px-4 py-2 cursor-pointer"
+            onClick={() => handleSelect("New events")}
           >
             <input type="radio" class="radio" id="newEvents" name="topic" />
             <label for="newEvents">New events</label>
           </div>
-          <div className="option px-4 py-2">
+          <div
+            className="option px-4 py-2 cursor-pointer"
+            onClick={() => handleSelect("Inspirations staf")}
+          >
             <input type="radio" class="radio" id="othertopic" name="topic" />
             <label for="newEvents">Inspirations staf</label>
           </div>
-          <div className="option px-4 py-2">
+          <div
+            className="option px-4 py-2 cursor-pointer"
+            onClick={() => handleSelect("All")}
+          >
             <input type="radio" class="radio" id="oneMore" name="topic" />
             <label for="newEvents">All</label>
           </div>
         </div>
         <div
-          className="selected-value"
+          className="selected-value cursor-pointer"
           onClick={() => setIsSelectActive((prev) => !prev)}
         >
           {selectValue}
