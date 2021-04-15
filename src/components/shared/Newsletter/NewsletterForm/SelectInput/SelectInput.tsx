04a0c0179e54da1708ba126889ecc70706970507
Kamil Sikora <sikorakamil26@gmail.com>
2021-04-15 14:17:14 +0200

    fix style

--- src/components/shared/Newsletter/NewsletterForm/SelectInput/SelectInput.tsx
+++ src/components/shared/Newsletter/NewsletterForm/SelectInput/SelectInput.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { BsChevronUp } from "react-icons/bs";
+import OutsideClickHandler from "react-outside-click-handler";
 import "./SelectInput.styled.css";
 import { UseFormRegister, UseFormSetValue } from "react-hook-form";
 import { FormData } from "../NewsletterForm";
+
 interface SelectInputProps {
   options: {
     name: string;
@@ -18,64 +20,57 @@ export const SelectInput = ({
   setValue,
 }: SelectInputProps) => {
   const [isSelectActive, setIsSelectActive] = useState(false);
-  const selectFieldRef = useRef(null);
 
-  const handleClickOutside = (event) => {
-    if (
-      selectFieldRef.current &&
-      !selectFieldRef.current.contains(event.target)
-    ) {
-      setIsSelectActive(false);
-    }
-  };
-  document.addEventListener("mousedown", handleClickOutside);
   const handleSelect = (value: string) => {
     setValue("topic", value);
     setIsSelectActive(false);
   };
 
   return (
-    <div
-      className="my-3 border border-customGrey border-opacity-50 rounded-md md:col-start-2 md:col-end-3 xl:mx-4"
-      ref={selectFieldRef}
-    >
-      <div className="select-box p-4 ">
-        <div
-          className={`options-container py-4 border border-opacity-50 border-customGrey rounded-md ${
-            isSelectActive ? "active" : ""
-          }`}
-        >
-          {options.map((option) => (
-            <div
-              className="option px-4 py-2 cursor-pointer"
-              key={option.name}
-              onClick={() => handleSelect(option.value)}
-            >
-              <input
-                type="radio"
-                className="radio"
-                id={option.name}
-                name="topic"
-              />
-              <label htmlFor={option.name}>{option.value}</label>
-            </div>
-          ))}
-        </div>
-        <div
-          className="selected-value cursor-pointer"
-          onClick={() => setIsSelectActive((prev) => !prev)}
-          {...register("topic")}
-        >
-          <input
-            name="topic"
-            placeholder="Topic"
+    <div className="my-3 border border-customGrey border-opacity-50 rounded-md md:col-start-2 md:col-end-3 xl:mx-4">
+      <OutsideClickHandler
+        onOutsideClick={() => {
+          setIsSelectActive(false);
+        }}
+      >
+        <div className="select-box p-4 ">
+          <div
+            className={`options-container py-4 border border-opacity-50 border-customGrey rounded-md ${
+              isSelectActive ? "active" : ""
+            }`}
+          >
+            {options.map((option) => (
+              <div
+                className="option px-4 py-2 cursor-pointer"
+                key={option.name}
+                onClick={() => handleSelect(option.value)}
+              >
+                <input
+                  type="radio"
+                  className="radio"
+                  id={option.name}
+                  name="topic"
+                />
+                <label htmlFor={option.name}>{option.value}</label>
+              </div>
+            ))}
+          </div>
+          <div
+            className="selected-value cursor-pointer"
+            onClick={() => setIsSelectActive((prev) => !prev)}
             {...register("topic")}
-            readOnly
-            className="outline-none"
-          />
-          <BsChevronUp className="stroke-1" />
+          >
+            <input
+              name="topic"
+              placeholder="Topic"
+              {...register("topic")}
+              readOnly
+              className="outline-none"
+            />
+            <BsChevronUp className="stroke-1" />
+          </div>
         </div>
-      </div>
+      </OutsideClickHandler>
     </div>
   );
 };
