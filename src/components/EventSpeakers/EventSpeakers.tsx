176ee77134bac4daae57c070bc2f7dc7dffc072d
Kamil Sikora <sikorakamil26@gmail.com>
2021-04-19 13:38:33 +0200

    refactor speakers to allow name

---
+++ src/components/EventSpeakers/EventSpeakers.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const EventSpeakers = ({ speakers }) => (
+  <div className="inner-grid">
+    <h2>Speakers</h2>
+  </div>
+);
---
+++ src/components/EventSpeakers/index.ts
@@ -0,0 +1,1 @@
+export { EventSpeakers } from "./EventSpeakers";
