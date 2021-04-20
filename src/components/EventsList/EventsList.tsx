0be4c2feaf103021f40b3512cfb618fad256ed36
Kamil Sikora <sikorakamil26@gmail.com>
2021-04-20 19:42:08 +0200

    remove unused imports

--- src/components/EventsList/EventsList.tsx
+++ src/components/EventsList/EventsList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { MainEvent } from "./MainEvent";
 import { EventsListProps } from "@/components/EventsList";
 import { ListFilters } from "@/components/EventsList/ListFilters";
