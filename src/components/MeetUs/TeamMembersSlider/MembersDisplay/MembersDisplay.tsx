8453ba0c83d7075a96116c5a4a50e94fe024e1d1
Kamil Sikora <sikorakamil26@gmail.com>
2021-04-22 10:38:26 +0200

    added correct markdown

---
+++ src/components/MeetUs/TeamMembersSlider/MembersDisplay/MembersDisplay.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { TeamMember } from "@/components/MeetUs/MeetUs.types";
+
+interface MembersDisplayProps {
+  teamMembers: TeamMember[];
+  activeMemberIndex: number;
+}
+
+export const MembersDisplay = ({
+  teamMembers,
+  activeMemberIndex,
+}: MembersDisplayProps) => (
+  <div className="flex text-white mb-10">
+    {teamMembers.map((member, index) => {
+      const name = member.name.split(" ");
+      return (
+        <div className="flex flex-col font-semibold w-28 mr-5">
+          <span>{name[0]}</span>
+          <span>{name[1]}</span>
+          <span
+            className={`h-1 bg-customRed rounded-sm w-full mt-3  ${
+              activeMemberIndex === index ? "opacity-100" : "opacity-50"
+            }`}
+          />
+        </div>
+      );
+    })}
+  </div>
+);
--- src/components/MeetUs/TeamMembersSlider/TeamMembersSlider.tsx
+++ src/components/MeetUs/TeamMembersSlider/TeamMembersSlider.tsx
@@ -1,26 +1,57 @@
 import React from "react";
+import { Slider, useSlider } from "@/components/shared/Slider";
+import ReactMarkdown from "react-markdown";
+import Img from "gatsby-image";
 import { TeamMember } from "@/components/MeetUs/MeetUs.types";
+import { MembersDisplay } from "./MembersDisplay";
+import "./TeamMembersSlider.styled.css";
 
 interface TeamMembersSliderProps {
   teamMembers: TeamMember[];
 }
 
 export const TeamMembersSlider = ({ teamMembers }: TeamMembersSliderProps) => {
+  const numberOfSlides = teamMembers.length;
+  const {
+    sliderRef,
+    inputSliderPosition,
+    inputSliderOnChange,
+    nextSlide,
+    prevSlide,
+    slider,
+    currentSlide,
+  } = useSlider(
+    numberOfSlides,
+    {
+      mobile: 1,
+      tablet: 1,
+      desktop: 1,
+    },
+    { mobile: 50, tablet: 50, desktop: 50 }
+  );
+
   return (
     <div>
       <h2>team members slide</h2>
-      <div className="flex text-white mb-10">
-        {teamMembers.map((member) => {
-          const name = member.name.split(" ");
-          return (
-            <div className="flex flex-col font-semibold w-28 mr-5">
-              <span>{name[0]}</span>
-              <span>{name[1]}</span>
-              <span className="h-1 bg-customRed rounded-sm w-full mt-3 opacity-50"></span>
+      <MembersDisplay teamMembers={teamMembers} activeMemberIndex={0} />
+      <Slider sliderRef={sliderRef}>
+        {teamMembers.map((member) => (
+          <div className="grid grid-cols-2 w-4/5">
+            <div>
+              <Img
+                className="h-full w-full z-0 background-image-only-desktop"
+                fluid={member.profileImage}
+                alt="Team member profile images"
+              />
             </div>
-          );
-        })}
-      </div>
+            <div className="p-5 bg-white team-member-details-container">
+              <h2 className="text-2xl font-semibold">{member.name}</h2>
+              <span className="text-2xl font-light">{member.title}</span>
+              <ReactMarkdown children={member.description} />
+            </div>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
