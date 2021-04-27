27c73c0578cd8cf7992ac2843029672c54ba1a8c
Kamil <sikorakamil26@gmail.com>
2021-04-27 13:55:07 +0200

    add no talks indicator

--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -38,6 +38,20 @@ export const Watch = ({
           filterTalks={filterTalks}
           eventNames={eventNames}
         />
+        {talksToShow.length === 0 && (
+          <div className="seamless-grid py-16">
+            <div className="col-start-2 col-end-3 md:col-start-2 md:col-end-5 xl:col-start-2 xl:col-end-5 pr-52">
+              <h2 className="text-2xl md:text-3xl">
+                Sorry. We couldn't find a talk quite like that.
+              </h2>
+              <p>
+                Looks like there weren't any talks matching your search
+                criteria. Try using a more general term or searching with fewer
+                filters
+              </p>
+            </div>
+          </div>
+        )}
       </div>
 
       <div className="pb-10">
