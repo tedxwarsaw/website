34c60778c7af1143e1259597a842dfca2a52cc0a
Kamil <sikorakamil26@gmail.com>
2021-04-29 12:51:23 +0200

    add sticky searchbar

--- src/components/Watch/SearchBar/SearchBar.tsx
+++ src/components/Watch/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SearchBarForm } from "./SearchBarForm";
 import { FilterList } from "./FilterList";
 import {
@@ -6,6 +6,7 @@ import {
   FilterTalks,
   SortTalks,
 } from "@/components/Watch/Watch.types";
+import "./SearchBar.styled.css";
 
 interface SearchBarProps {
   activeFilters: ActiveFilters;
@@ -21,19 +22,39 @@ export const SearchBar = ({
   filterTalks,
   sortTalks,
   eventNames,
-}: SearchBarProps) => (
-  <div className="seamless-grid">
-    <SearchBarForm
-      eventNames={eventNames}
-      filterTalks={filterTalks}
-      activeFilters={activeFilters}
-    />
-    <FilterList
-      activeFilters={activeFilters}
-      filterTalks={filterTalks}
-      eventNames={eventNames}
-      activeSorting={activeSorting}
-      sortTalks={sortTalks}
-    />
-  </div>
-);
+}: SearchBarProps) => {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== `undefined`) {
+      window.onscroll = () => {
+        setOffset(window.pageYOffset);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(offset);
+  }, [offset]);
+
+  return (
+    <div
+      className={`seamless-grid bg-customLightGrey duration-300 ${
+        offset >= 260 ? "sticky-search-bar shadow-md" : ""
+      }`}
+    >
+      <SearchBarForm
+        eventNames={eventNames}
+        filterTalks={filterTalks}
+        activeFilters={activeFilters}
+      />
+      <FilterList
+        activeFilters={activeFilters}
+        filterTalks={filterTalks}
+        eventNames={eventNames}
+        activeSorting={activeSorting}
+        sortTalks={sortTalks}
+      />
+    </div>
+  );
+};
