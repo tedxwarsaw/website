a48d5886fe74f8d45e839444ce56e32c08f7cc02
Kamil <sikorakamil26@gmail.com>
2021-05-20 10:31:13 +0200

    add query params to search

--- src/components/Watch/Watch.hooks.ts
+++ src/components/Watch/Watch.hooks.ts
@@ -1,6 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { usePagination } from "@/components/shared/Pagination";
 import { durationFilters } from "@/components/Watch/helpers";
+import qs from "query-string";
+
+const updateQueryParams = (queryParams) => {
+  var newurl =
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    window.location.pathname +
+    "?" +
+    queryParams;
+  window.history.pushState({ path: newurl }, "", newurl);
+};
 
 export const useWatch = (talks) => {
   const [filteredTalks, setFilteredTalks] = useState(talks);
@@ -14,21 +26,24 @@ export const useWatch = (talks) => {
     filteredTalks,
     21
   );
+  const params = qs.parse(window.location.search);
 
   const filterTalks = (
     searchPhrase: string,
     eventSlug: string,
     durationFilter: string
   ) => {
-    setActiveFilters({
+    const filters = {
       searchPhrase: searchPhrase,
       eventSlug: eventSlug,
       durationFilter: durationFilter,
-    });
+    };
+
+    setActiveFilters(filters);
 
     let talksFiltered = [...talks];
 
-    if (searchPhrase !== "") {
+    if (searchPhrase) {
       talksFiltered = talksFiltered.filter((talk) =>
         Object.values(talk).some((val: string) => {
           if (typeof val === "string") {
@@ -39,18 +54,22 @@ export const useWatch = (talks) => {
         })
       );
     }
-    if (eventSlug !== "") {
+    if (eventSlug) {
       talksFiltered = talksFiltered.filter(
         (talk) => talk.eventSlug === eventSlug
       );
     }
 
-    if (durationFilter !== "") {
+    if (durationFilter) {
       talksFiltered = talksFiltered.filter((talk) =>
         durationFilters[durationFilter](talk.duration)
       );
     }
     setFilteredTalks(talksFiltered);
+
+    const params = qs.stringify(filters);
+    updateQueryParams(params);
+    // location.search = params;
   };
 
   const sortTalks = (sortType) => {
@@ -65,6 +84,15 @@ export const useWatch = (talks) => {
       setFilteredTalks(talksSorted);
     }
   };
+
+  useEffect(() => {
+    console.log(activeFilters);
+    filterTalks(
+      params?.searchPhrase,
+      params?.eventSlug,
+      params?.durationFilter
+    );
+  }, []);
 
   return {
     activeFilters,
