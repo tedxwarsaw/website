351fbf7d3eb306296f2f8be8f47377a3c1f6cefe
Kamil <sikorakamil26@gmail.com>
2021-05-21 09:52:04 +0200

    fix ssr

--- src/hooks/useQueryParameters.ts
+++ src/hooks/useQueryParameters.ts
@@ -1,17 +1,23 @@
+import { useState, useEffect } from "react";
 import qs from "query-string";
 
 export const useQueryParameters = () => {
-  const params = qs.parse(window.location.search);
+  const [params, setParams] = useState({});
+
+  useEffect(() => {
+    const queryParams = qs.parse(window?.location.search);
+    setParams(queryParams);
+  });
 
   const updateQueryParams = (queryParams) => {
     var newurl =
-      window.location.protocol +
+      window?.location.protocol +
       "//" +
-      window.location.host +
-      window.location.pathname +
+      window?.location.host +
+      window?.location.pathname +
       "?" +
       queryParams;
-    window.history.pushState({ path: newurl }, "", newurl);
+    window?.history.pushState({ path: newurl }, "", newurl);
   };
 
   return {
