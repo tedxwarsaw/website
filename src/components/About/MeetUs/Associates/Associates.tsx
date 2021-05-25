fee674b6f239d154c11ce109044c5b4d4d087558
Łukasz Skup <lukasz.skup1999@gmail.com>
2021-05-25 17:35:51 +0200

    quick-fix delete background image from slider

--- src/components/About/MeetUs/Associates/Associates.tsx
+++ src/components/About/MeetUs/Associates/Associates.tsx
@@ -8,8 +8,8 @@ interface AssociatesProps {
 
 export const Associates = ({ associates }: AssociatesProps) => (
   <>
-    <h3 className="text-white text-lg font-semibold">Associates:</h3>
-    <div className="col-start-1 col-end-full grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 grid-flow-row gap-20 my-10">
+    <h3 className="text-white font-semibold">Associates:</h3>
+    <div className="col-start-1 col-end-full grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 grid-flow-row gap-20 my-10 text-3xl">
       {associates.map((associate, index) => (
         <div
           key={associate.name + index}
