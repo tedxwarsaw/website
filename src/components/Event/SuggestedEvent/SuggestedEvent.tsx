30fb5ce9aad351ad2947a89ee05703c7630f8c13
Kamil <sikorakamil26@gmail.com>
2021-06-01 20:36:54 +0200

    minor fixes

--- src/components/Event/SuggestedEvent/SuggestedEvent.tsx
+++ src/components/Event/SuggestedEvent/SuggestedEvent.tsx
@@ -4,6 +4,7 @@ import { HiMenuAlt2 } from "react-icons/hi";
 import { Link } from "gatsby";
 import Img, { FluidObject } from "gatsby-image";
 import { SuggestedEventSlider } from "./SuggestedEventSlider";
+import "./SuggestedEvent.styled.css";
 
 interface SuggestedEventProps {
   displayName: string;
@@ -19,35 +20,37 @@ export const SuggestedEvent = ({
   return (
     <div className="main-grid-full-span py-20 space-y-4">
       <div className="seamless-grid">
-        <div className="space-y-4 col-start-2 col-end-3">
-          <div className="text-4xl font-bold">{displayName}</div>
-          <div className="text-red-500 flex space-x-6">
-            <Link
-              to={`/watch?event=${slug}`}
-              className="font-semibold hover:opacity-50 flex items-center"
-            >
-              Watch the talks {'->'}
-            </Link>
-            <Link
-              to={`/event/${slug}/`}
-              className="font-semibold hover:opacity-50 flex items-center"
-            >
-              Read more <HiMenuAlt2 className="ml-2 text-xl" />
-            </Link>
+        <div className="col-start-2 col-span-full">
+          <div className="space-y-4 col-start-2 col-end-3">
+            <div className="text-4xl font-bold">{displayName}</div>
+            <div className="text-red-500 flex space-x-6">
+              <Link
+                to={`/watch?event=${slug}`}
+                className="font-semibold hover:opacity-50 flex items-center"
+              >
+                Watch the talks {"->"}
+              </Link>
+              <Link
+                to={`/event/${slug}/`}
+                className="font-semibold hover:opacity-50 flex items-center"
+              >
+                Read more <HiMenuAlt2 className="ml-2 text-xl" />
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="col-start-2 flex flex-col flew-nowrap col-span-full">
-          <SuggestedEventSlider>
-            {photos.map((fluid, idx) => (
-              <div key={idx} className="h-full">
-                <Img
-                  fluid={fluid}
-                  className="h-full w-full"
-                  alt={`Photo from ${displayName}`}
-                />
-              </div>
-            ))}
-          </SuggestedEventSlider>
+          <div className="flex flex-col flew-nowrap  suggested-event-slider-container">
+            <SuggestedEventSlider>
+              {photos.map((fluid, idx) => (
+                <div key={idx} className="h-full">
+                  <Img
+                    fluid={fluid}
+                    className="h-full w-full"
+                    alt={`Photo from ${displayName}`}
+                  />
+                </div>
+              ))}
+            </SuggestedEventSlider>
+          </div>
         </div>
       </div>
     </div>
