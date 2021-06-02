f661e95c4922eddea476c02c83e83f9676131f55
Kamil <sikorakamil26@gmail.com>
2021-06-02 14:29:47 +0200

    fixed widht of slider

--- src/components/About/MeetUs/TeamMembersSlider/TeamMembersSlider.tsx
+++ src/components/About/MeetUs/TeamMembersSlider/TeamMembersSlider.tsx
@@ -39,52 +39,56 @@ export const TeamMembersSlider = ({ teamMembers }: TeamMembersSliderProps) => {
         </div>
       </div>
       <div className="main-grid-full-span relative mb-10">
-        <div className="relative main-grid z-10 team-members-slider-container">
-          <Slider sliderRef={sliderRef}>
-            {teamMembers.map((member, index) => (
-              <div
-                className="flex flex-col md:grid md:grid-cols-2 md:grid-cols-2 w-full"
-                key={member.name + index}
-              >
-                <div className="h-64 md:h-auto">
-                  <Img
-                    className="h-full w-full z-0 md:hidden"
-                    fluid={member.profileImage}
-                    alt="Team member profile images"
-                  />
-                  <Img
-                    className="h-full w-full z-0 hidden md:block"
-                    fluid={member.profileImageDesktop}
-                    alt="Team member profile images"
-                  />
+        <div className="relative seamless-grid z-10 team-members-slider-container">
+          <div className="col-start-2 col-span-full relative">
+            <Slider sliderRef={sliderRef}>
+              {teamMembers.map((member, index) => (
+                <div
+                  className="flex flex-col md:grid md:grid-cols-2 md:grid-cols-2 w-full"
+                  key={member.name + index}
+                >
+                  <div className="h-64 md:h-auto border-none">
+                    <Img
+                      className=" w-full z-0 md:hidden"
+                      fluid={member.profileImage}
+                      alt="Team member profile images"
+                    />
+                    <Img
+                      className="h-full w-full z-0 hidden md:block"
+                      fluid={member.profileImageDesktop}
+                      alt="Team member profile images"
+                    />
+                  </div>
+                  <div className="p-5 bg-white team-member-details-container">
+                    <h2 className="font-semibold">{member.name}</h2>
+                    <span className="block text-2xl font-light mb-10">
+                      {member.title}
+                    </span>
+                    <ReactMarkdown children={member.description} />
+                  </div>
                 </div>
-                <div className="p-5 bg-white team-member-details-container">
-                  <h2 className="font-semibold">{member.name}</h2>
-                  <span className="block text-2xl font-light mb-10">
-                    {member.title}
-                  </span>
-                  <ReactMarkdown children={member.description} />
-                </div>
-              </div>
-            ))}
-          </Slider>
-          <div
-            className={`team-members-slider-controls control-left cursor-pointer ${
-              currentSlide === 0 ? "hidden" : "flex justify-center items-center"
-            }`}
-            onClick={prevSlide}
-          >
-            <BsChevronLeft className="text-customRed hover:opacity-40 cursor-pointer stroke-1 text-3xl " />
-          </div>
-          <div
-            className={`team-members-slider-controls cursor-pointer control-right ${
-              currentSlide === numberOfSlides - 1
-                ? "hidden"
-                : "flex justify-center items-center"
-            }`}
-            onClick={nextSlide}
-          >
-            <BsChevronRight className="text-customRed hover:opacity-40 cursor-pointer stroke-1 text-3xl" />
+              ))}
+            </Slider>
+            <div
+              className={`team-members-slider-controls control-left cursor-pointer ${
+                currentSlide === 0
+                  ? "hidden"
+                  : "flex justify-center items-center"
+              }`}
+              onClick={prevSlide}
+            >
+              <BsChevronLeft className="text-customRed hover:opacity-40 cursor-pointer stroke-1 text-3xl " />
+            </div>
+            <div
+              className={`team-members-slider-controls cursor-pointer control-right ${
+                currentSlide === numberOfSlides - 1
+                  ? "hidden"
+                  : "flex justify-center items-center"
+              }`}
+              onClick={nextSlide}
+            >
+              <BsChevronRight className="text-customRed hover:opacity-40 cursor-pointer stroke-1 text-3xl" />
+            </div>
           </div>
         </div>
       </div>
