8e4714f12cc2e34a37c917d235881881799e0dec
Łukasz Skup <lukasz.skup1999@gmail.com>
2021-06-07 17:18:01 +0200

    fixed process line on mobile in HowDoesItWork Component

--- src/components/Speakers/HowDoesItWork/HowDoesItWork.tsx
+++ src/components/Speakers/HowDoesItWork/HowDoesItWork.tsx
@@ -20,10 +20,14 @@ export const HowDoesItWork = (props: HowDoesItWorkProps) => (
         {props.howDoesItWorkTitle}
       </ReactMarkdown>
     </div>
-    <div className="col-start-2 col-end-3 mt-8 md:col-start-2 md:col-end-5 md:mt-8 xl:col-start-4 xl:col-end-7 xl:mt-0 font-normal steps pl-6">
-      <ProgressLine />
-      {props.howDoesItWorkSteps.map((step: Step) => (
-        <div className={"step"}>{step.name}</div>
+    <div className="col-start-2 col-end-3 mt-8 md:col-start-2 md:col-end-5 md:mt-8 xl:col-start-4 xl:col-end-7 xl:mt-0 font-normal steps">
+      {/*<ProgressLine />*/}
+      {props.howDoesItWorkSteps.map((step: Step, index: number) => (
+          <div className={"step"}>
+              {index == 0 && <span className={"dot-element"}/>}
+              {index == props.howDoesItWorkSteps.length - 1 && <span className={"dot-element-two"}/>}
+              {step.name}
+          </div>
       ))}
     </div>
   </div>
