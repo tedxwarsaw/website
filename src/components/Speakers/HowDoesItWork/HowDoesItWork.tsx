a5b5a247ac0dc024dcd2acee8f45e8296f06ec01
Hugo Dutka <contact@hugodutka.com>
2021-07-18 16:54:44 +0200

    clean up the code

--- src/components/Speakers/HowDoesItWork/HowDoesItWork.tsx
+++ src/components/Speakers/HowDoesItWork/HowDoesItWork.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import rehypeRaw from "rehype-raw";
 import ReactMarkdown from "react-markdown";
 import "./HowDoesItWork.styled.css";
-import { ProgressLine } from "./ProgressLine";
 
 interface Step {
   name: string;
@@ -21,13 +20,14 @@ export const HowDoesItWork = (props: HowDoesItWorkProps) => (
       </ReactMarkdown>
     </div>
     <div className="col-start-2 col-end-3 mt-8 md:col-start-2 md:col-end-5 md:mt-8 xl:col-start-4 xl:col-end-7 xl:mt-0 font-normal steps">
-      {/*<ProgressLine />*/}
       {props.howDoesItWorkSteps.map((step: Step, index: number) => (
-          <div className={"step"}>
-              {index == 0 && <span className={"dot-element"}/>}
-              {index == props.howDoesItWorkSteps.length - 1 && <span className={"dot-element-two"}/>}
-              {step.name}
-          </div>
+        <div className={"step"}>
+          {index == 0 && <span className={"dot-element"} />}
+          {index == props.howDoesItWorkSteps.length - 1 && (
+            <span className={"dot-element-two"} />
+          )}
+          {step.name}
+        </div>
       ))}
     </div>
   </div>
