2f3ed6c653a7523fb804f6887db412246c14e332
Kamil <sikorakamil26@gmail.com>
2021-09-02 12:39:11 +0200

    our story fixes

--- src/components/Index/OurStory/OurStory.tsx
+++ src/components/Index/OurStory/OurStory.tsx
@@ -1,18 +1,49 @@
 import React from "react";
 import { OurStoryProps } from "./OurStory.types";
-import {Lines} from "../../Lines/Lines";
+import { Lines } from "../../Lines/Lines";
 
 export const OurStory = ({ ourStoryTitle, ourStoryItems }: OurStoryProps) => (
   <div className="bg-customLightGrey main-grid-full-span py-10 relative">
-    <Lines onlyHorizontal={true}/>
+    <Lines onlyHorizontal={true} />
     <div className="main-grid">
-      <div className="flex flex-wrap justify-center">
-        <div className="px-8 py-4">{ourStoryTitle}</div>
-        {ourStoryItems.map(({ boldText, text }, i) => (
-          <div className="px-8 py-4" key={i}>
-            <b>{boldText}</b> {text}
-          </div>
-        ))}
+      <div className="inner-grid">
+        <div className="pr-8 py-4 flex">
+          <p>{ourStoryTitle}</p>
+          {ourStoryItems.map(({ boldText, text }, i) => {
+            if (i % 4 !== 0) return;
+            return (
+              <div className="px-8" key={i}>
+                <p>
+                  <b>{boldText}</b> {text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="px-8 py-4 flex">
+          {ourStoryItems.map(({ boldText, text }, i) => {
+            if (i % 4 !== 1 && i % 4 !== 2) return;
+            return (
+              <div className="pr-8" key={i}>
+                <p>
+                  <b>{boldText}</b> {text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="px-8 py-4 flex">
+          {ourStoryItems.map(({ boldText, text }, i) => {
+            if (i % 4 !== 3) return;
+            return (
+              <div className="pr-8" key={i}>
+                <p>
+                  <b>{boldText}</b> {text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   </div>
--- src/components/Lines/Lines.tsx
+++ src/components/Lines/Lines.tsx
@@ -2,27 +2,29 @@ import React from "react";
 import "./Lines.styled.css";
 
 interface ILinesProps {
-    onlyHorizontal?: boolean;
-    wider?: boolean;
+  onlyHorizontal?: boolean;
+  wider?: boolean;
 }
 
-export const Lines = ({onlyHorizontal, wider}) => (
-    <div className={`lines ${wider ? 'wider' : ''}`}>
-        <div className="main-grid">
-            <div className={"col-start-3 col-end-6 my-10 inner-grid gap-5 lines-grid"}>
-                <div className={"line-container"}>
-                    <div className={"guide-line"}/>
-                    <div className={`guide-line ${onlyHorizontal ? 'hidden' : ''}`}/>
-                </div>
-                <div className={`line-container`}>
-                    <div className={`guide-line ${onlyHorizontal ? 'hidden' : ''}`}/>
-                    <div className={`guide-line ${onlyHorizontal ? 'hidden' : ''}`}/>
-                </div>
-                <div className={`line-container`}>
-                    <div className={`guide-line ${onlyHorizontal ? 'hidden' : ''}`}/>
-                    <div className={"guide-line"}/>
-                </div>
-            </div>
+export const Lines = ({ onlyHorizontal, wider }: ILinesProps) => (
+  <div className={`lines ${wider ? "wider" : ""}`}>
+    <div className="main-grid">
+      <div
+        className={"col-start-3 col-end-6 my-10 inner-grid gap-5 lines-grid"}
+      >
+        <div className={"line-container"}>
+          <div className={"guide-line"} />
+          <div className={`guide-line ${onlyHorizontal ? "hidden" : ""}`} />
         </div>
+        <div className={`line-container`}>
+          <div className={`guide-line ${onlyHorizontal ? "hidden" : ""}`} />
+          <div className={`guide-line ${onlyHorizontal ? "hidden" : ""}`} />
+        </div>
+        <div className={`line-container`}>
+          <div className={`guide-line ${onlyHorizontal ? "hidden" : ""}`} />
+          <div className={"guide-line"} />
+        </div>
+      </div>
     </div>
+  </div>
 );
