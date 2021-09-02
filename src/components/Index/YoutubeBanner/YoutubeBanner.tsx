602b6ccb39efb892f323f63bf022f1de66845496
Kamil <sikorakamil26@gmail.com>
2021-09-02 11:32:36 +0200

    fix buttons

--- src/components/Index/YoutubeBanner/YoutubeBanner.tsx
+++ src/components/Index/YoutubeBanner/YoutubeBanner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, ButtonVariant } from "@/components/shared/Button";
 import parse from "html-react-parser";
-import {Lines} from "../../Lines/Lines";
+import { Lines } from "../../Lines/Lines";
 import { Link } from "gatsby";
 
 export interface YoutubeBannerProps {
@@ -16,21 +16,18 @@ export const YoutubeBanner = ({
   youtubeBannerLinkUrl,
 }: YoutubeBannerProps) => {
   return (
-      <div className={"relative main-grid-full-span"}>
-        <Lines onlyHorizontal={true}/>
-        <div className="my-16 text-center flex flex-col justify-center align-center">
-          <h2 className="font-medium mb-6 mx-auto md:w-96">
-            {parse(youtubeBannerHeading)}
-          </h2>
-          <Link to={youtubeBannerLinkUrl} className="mb-5 md:mb-0">
-            <Button
-              variant={ButtonVariant.filledGrey}
-              className={"hover:bg-white hover:text-customRed"}
-            >
-              {youtubeBannerLinkText}
-            </Button>
-          </Link>
-        </div>
+    <div className={"relative main-grid-full-span"}>
+      <Lines onlyHorizontal={true} />
+      <div className="my-16 text-center flex flex-col justify-center align-center">
+        <h2 className="font-medium mb-6 mx-auto md:w-96">
+          {parse(youtubeBannerHeading)}
+        </h2>
+        <Link to={youtubeBannerLinkUrl} className="mb-5 md:mb-0">
+          <Button variant={ButtonVariant.filledGrey}>
+            {youtubeBannerLinkText}
+          </Button>
+        </Link>
       </div>
+    </div>
   );
 };
